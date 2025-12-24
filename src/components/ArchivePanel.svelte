5588d43e8ffa21da719e3540ffe1f8fc85e0a1ed
HAOHAOHAOHAOHAO <15180147+haohaohaohaohaooo@user.noreply.gitee.com>
2025-12-25 00:24:20 +0800

    final

--- src/components/ArchivePanel.svelte
+++ src/components/ArchivePanel.svelte
@@ -4,29 +4,20 @@ import { onMount } from "svelte";
 import I18nKey from "../i18n/i18nKey";
 import { i18n } from "../i18n/translation";
 import { getPostUrlBySlug } from "../utils/url-utils";
+import type { PostForList } from "../utils/content-utils";
 
 export let tags: string[];
 export let categories: string[];
-export let sortedPosts: Post[] = [];
+export let sortedPosts: PostForList[] = [];
 
 const params = new URLSearchParams(window.location.search);
 tags = params.has("tag") ? params.getAll("tag") : [];
 categories = params.has("category") ? params.getAll("category") : [];
 const uncategorized = params.get("uncategorized");
 
-interface Post {
-	slug: string;
-	data: {
-		title: string;
-		tags: string[];
-		category?: string;
-		published: Date;
-	};
-}
-
 interface Group {
 	year: number;
-	posts: Post[];
+	posts: PostForList[];
 }
 
 let groups: Group[] = [];
